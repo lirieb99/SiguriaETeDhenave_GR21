cb50e9c1e72d167e8756cd1b3ee58b783d26e353
Valon Pllana <valonpllana28@gmail.com>
2020-06-07 19:47:27 +0200

    delete user

---
+++ ds/ds/DeleteUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace ds
+{
+    public class DeleteUser
+    {
+        DatabaseConnection DB = new DatabaseConnection();
+
+        public void DeleteRsaKey(string privateKeyPath, string publicKeyPath)
+        {
+            File.Delete(privateKeyPath);
+            File.Delete(publicKeyPath);
+        }
+
+        public void DeleteRsaKey(string keyPath)
+        {
+            File.Delete(keyPath);
+        }
+
+        public void DeletefromDB(string username)
+        {
+            try
+            {
+
+                String query = "DELETE FROM users WHERE USERNAME=" + "'" + username + "';";
+
+                DB.Open();
+                SqlDataReader row;
+                row = DB.ExecuteReader(query);
+                Console.WriteLine("Eshte fshire shfrytezuesi " + username);
+                DB.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
